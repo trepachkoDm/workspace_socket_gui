08178a376759c5b01456b38e6576921840a0bbb4
Dm <tr@Dms-MacBook-Air.local>
2023-04-03 21:38:56 +0300

    add c

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -32,3 +32,50 @@ impl SocketClient {
         Ok(())
     }
 }
+
+#[repr(i32)]
+pub enum SockError {
+    NoError = 0,
+    SwitchOnFailed,
+    SwitchOffFailed,
+    GetStatusFailed,
+}
+
+#[derive(Default)]
+#[repr(C)]
+pub struct SocketState {
+    state: u32,
+    power: f64,
+    error: i32,
+}
+
+#[no_mangle]
+pub extern "C" fn sync() -> SocketState {
+    match SocketClient::get_state() {
+        Ok((state, power)) => SocketState {
+            state: state as u32,
+            power,
+            error: SockError::NoError as i32,
+        },
+        Err(_) => SocketState {
+            error: SockError::GetStatusFailed as i32,
+            ..Default::default()
+        },
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn switch_on() -> SockError {
+    match SocketClient::switch_on() {
+        Ok(_) => SockError::NoError,
+        Err(_) => SockError::SwitchOnFailed,
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn switch_off() -> SockError {
+    match SocketClient::switch_off() {
+        Ok(_) => SockError::NoError,
+        Err(_) => SockError::SwitchOffFailed,
+    }
+}
